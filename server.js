593f478fd0b629a56a3fad922a58966f2451c391
sneha gourishetty <gourishettysneha@gmail.com>
2023-09-15 01:09:48 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,29 +4,12 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-        {
-            title: 'Eat out',
-            budget: 30
-        },
-        {
-            title: 'Rent',
-            budget: 350
-        },
-        {
-            title: 'Groceries',
-            budget: 90
-        }
-    ]
-};
-
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.sendFile("C:\\Users\\sneha\\Documents\\NBAD\\personal-budget\\personal-budget.json");
 })
 
 app.listen(port, () => {
